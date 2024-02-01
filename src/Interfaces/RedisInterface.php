fc5193c2133f289c159b11a04944565e3debc43f
Simon <contact@smn.codes>
2024-02-01 21:49:42 +0100

    Comments

--- src/Interfaces/RedisInterface.php
+++ src/Interfaces/RedisInterface.php
@@ -36,19 +36,61 @@ interface RedisInterface
 {
     public function __construct(mixed $parameters = null, mixed $options = null);
 
+    /**
+     * Returns information and statistics about the server.
+     *
+     * @see https://redis.io/commands/info/
+     */
     public function info(mixed $section = null): array;
 
+    /**
+     * Returns all keys matching `$pattern`.
+     *
+     * @see https://redis.io/commands/keys/
+     */
     public function keys(string $pattern): array;
 
+    /**
+     * Returns the length of the list stored at `$key`.
+     *
+     * @see https://redis.io/commands/llen/
+     */
     public function llen(string $key): int;
 
+    /**
+     * Insert all the specified values at the head of the list stored at `$key`.
+     *
+     * @see https://redis.io/commands/lpush/
+     */
     public function lpush(string $key, array $values): int;
 
+    /**
+     * Removes and returns the last elements of the list stored at `$key`.
+     *
+     * @see https://redis.io/commands/rpop/
+     */
     public function rpop(string $key): string|null;
 
+    /**
+     * The blocking version of `rpop()` because it blocks the connection when
+     * there are no elements to pop from any of the given lists.
+     *
+     * @see https://redis.io/commands/brpop/
+     */
     public function brpop(array|string $keys, float|int $timeout): array|null;
 
+    /**
+     * Removes the specified keys. A key is ignored if it does not exist.
+     *
+     * @see https://redis.io/commands/del/
+     */
     public function delete(array|string $keyOrKeys, string|null ...$keys): int;
 
+    /**
+     * Set a timeout on key. After the timeout has expired,
+     * the key will automatically be deleted.
+     *
+     * @seehttps://redis.io/commands/expire/
+     */
     public function expire(string $key, int $seconds, string|null $expireOption = ''): int;
 }
